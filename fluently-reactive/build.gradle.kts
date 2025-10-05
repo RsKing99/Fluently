0741df3a57c7d70a23d5828785ea16255ff34c78
KitsuneAlex <alexh@karmakrafts.dev>
2025-10-05 22:10:39 +0200

    1.3.2 - Add all missing Apple build targets

--- fluently-reactive/build.gradle.kts
+++ fluently-reactive/build.gradle.kts
@@ -49,6 +49,14 @@ kotlin {
     androidNativeArm32()
     iosX64()
     iosArm64()
+    iosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    watchosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+    tvosSimulatorArm64()
     js {
         browser()
     }
